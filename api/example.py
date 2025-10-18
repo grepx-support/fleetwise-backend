d38bcf8e20137217b7e283d49992ddd7c788134f
Sridevi Sundararajan <support@grepx.sg>
2025-10-18 08:34:05 +0800

    G1002-215  moved everything inside backend

--- api/example.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-example_bp = Blueprint('example', __name__)
- 
-@example_bp.route('/hello', methods=['GET'])
-def hello():
-    return jsonify({'message': 'Hello from the new backend API!'}) 
--- schemas/role_schema.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields
-
-class RoleSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-    description = fields.Str()
-    users = fields.List(fields.Nested(lambda: UserSchema(exclude=("roles",)), dump_only=True))
-
-from backend.schemas.user_schema import UserSchema  # Avoid circular import 
